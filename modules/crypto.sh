b24312697b57458f426ffc21fb245619e40065d1
Artto <147250571+arrrtto@users.noreply.github.com>
2025-08-02 21:27:37 +0300

    Update crypto.sh
    Removed qort_price() due to TradeOgre exchange being down.

--- modules/crypto.sh
+++ modules/crypto.sh
@@ -10,11 +10,3 @@ function crypto_fearandgreedindex() {
 # Gets the current crypto fear and greed index via API.
 curl -s "https://api.alternative.me/fng/" | jq -r '.data[0] | "Fear & Greed Index: \(.value) (\(.value_classification))"'
 }
-
-
-function qort_price() {
-# Gets the current market price of QORT cryptocurrency from TradeOgre using API.
-curl -sX GET "https://tradeogre.com/api/v1/markets" | jq -r '.[]["QORT-USDT"].price // empty | tonumber'
-}
-
-
